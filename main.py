c5b06603141986852bbd868e77d9907a91cd95e9
rogerboroda <roger_boroda@mail.ru>
2024-10-17 19:56:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):  # - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>"
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('"Такого этажа не существует"')
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# # __len__
+print(len(h1))
+print(len(h2))
